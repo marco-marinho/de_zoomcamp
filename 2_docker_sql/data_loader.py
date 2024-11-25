85eb88ba1e589f2b54367820cc2cf610498f35d6
Marco Marinho <marcoobom@gmail.com>
2024-11-25 21:21:18 +0100

    Docker compose

--- 2_docker_sql/data_loader.py
+++ 2_docker_sql/data_loader.py
@@ -6,15 +6,29 @@ app = typer.Typer()
 
 
 @app.command()
-def load_data(user: str, passwd: str, host: str, port: int, db: str, parquet_file: str):
+def load_data(
+    user: str,
+    passwd: str,
+    host: str,
+    port: int,
+    db: str,
+    parquet_file: str,
+    csv_file: str,
+):
     # Create a connection to the database
     engine = create_engine(f"postgresql://{user}:{passwd}@{host}:{port}/{db}")
 
     # Load the data from the parquet file
-    df_iter = pd.read_parquet(parquet_file)
+    df = pd.read_parquet(parquet_file)
 
     # Write the data to the database
-    df_iter.to_sql("yellow_taxi_data", con=engine, if_exists="replace")
+    df.to_sql("yellow_taxi_data", con=engine, if_exists="replace")
+
+    # Load the zone lookup data
+    df = pd.read_csv(csv_file)
+
+    # Write the data to the database
+    df.to_sql("taxi_zone_lookup", con=engine, if_exists="replace")
 
 
 if __name__ == "__main__":
